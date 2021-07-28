a8225936051669417fd41026d3060ca09905073c
J@tin <ezedin.fedlu@gmail.com>
2021-07-29 01:56:24 +0300

    fix auth

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -7,20 +7,20 @@ import { UserService } from 'src/modules/user/user.service';
 import { cookieNames } from '../constants';
 @Injectable()
 export default class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-    public constructor(
-        private readonly userService: UserService,
-        protected readonly configService: ConfigService,
-    ) {
-        super({
-            secretOrKey: configService.get<string>('JWT_ACCESS_TOKEN_SECRET'),
-            issuer: configService.get<string>('JWT_ISSUER'),
-            audience: configService.get<string>('JWT_AUDIENCE'),
-            jwtFromRequest: ExtractJwt.fromExtractors([
-                ExtractJwt.fromAuthHeaderAsBearerToken()
-            ]),
-        });
-    }
-    async validate(payload: any) {
-        return await this.userService.findById(payload.sub);
-    }
+  public constructor(
+    private readonly userService: UserService,
+    protected readonly configService: ConfigService,
+  ) {
+    super({
+      secretOrKey: configService.get<string>('JWT_ACCESS_TOKEN_SECRET'),
+      issuer: configService.get<string>('JWT_ISSUER'),
+      audience: configService.get<string>('JWT_AUDIENCE'),
+      jwtFromRequest: ExtractJwt.fromExtractors([
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ]),
+    });
+  }
+  async validate(payload: any) {
+    return await this.userService.findById(String(payload.sub));
+  }
 }
