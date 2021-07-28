e44713017e5cbbf881ce6902d6a2aadb3e4281fd
J@tin <ezedin.fedlu@gmail.com>
2021-07-29 01:10:39 +0300

    fix auth

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,42 +1,57 @@
-import { Module } from "@nestjs/common";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { JwtService } from "@nestjs/jwt";
-import { UserModule } from "../user/user.module";
-import { AuthController } from "./auth.controller";
-import { JWT_ACCESS_TOKEN_SERVICE, JWT_REFRESH_TOKEN_SERVICE } from "./constants";
-import { AuthService } from "./services/auth.service";
-import { TokenService } from "./services/token.service";
-import JwtRefreshStrategy from "./strategies/jwt-refresh.strategy";
-import JwtStrategy from "./strategies/jwt.strategy";
-const jwtServiceFactory = (secret, signOptions = {}) => new JwtService({
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UserModule } from '../user/user.module';
+import { AuthController } from './auth.controller';
+import {
+  JWT_ACCESS_TOKEN_SERVICE,
+  JWT_REFRESH_TOKEN_SERVICE,
+} from './constants';
+import { AuthService } from './services/auth.service';
+import { TokenService } from './services/token.service';
+import JwtRefreshStrategy from './strategies/jwt-refresh.strategy';
+import JwtStrategy from './strategies/jwt.strategy';
+const jwtServiceFactory = (secret, signOptions = {}) =>
+  new JwtService({
     secret,
-    signOptions
-})
+    signOptions,
+  });
 @Module({
-    controllers: [AuthController],
-    imports: [
-        ConfigModule,
-        UserModule
-    ],
-    providers: [
-        TokenService,
-        AuthService,
-        JwtStrategy,
-        JwtRefreshStrategy,
-        {
-            provide: JWT_ACCESS_TOKEN_SERVICE,
-            useFactory: (configService: ConfigService): JwtService => {
-                return jwtServiceFactory(configService.get<string>("JWT_ACCESS_TOKEN_SECRET"))
-            },
-            inject: [ConfigService],
-        },
-        {
-            provide: JWT_REFRESH_TOKEN_SERVICE,
-            useFactory: (configService: ConfigService): JwtService => {
-                return jwtServiceFactory(configService.get<string>("JWT_REFRESH_TOKEN_SECRET"))
-            },
-            inject: [ConfigService],
-        },
-    ]
+  controllers: [AuthController],
+  imports: [
+    ConfigModule,
+    UserModule,
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+      property: 'user',
+      session: false,
+    }),
+  ],
+  providers: [
+    TokenService,
+    AuthService,
+    JwtStrategy,
+    JwtRefreshStrategy,
+
+    {
+      provide: JWT_ACCESS_TOKEN_SERVICE,
+      useFactory: (configService: ConfigService): JwtService => {
+        return jwtServiceFactory(
+          configService.get<string>('JWT_ACCESS_TOKEN_SECRET'),
+        );
+      },
+      inject: [ConfigService],
+    },
+    {
+      provide: JWT_REFRESH_TOKEN_SERVICE,
+      useFactory: (configService: ConfigService): JwtService => {
+        return jwtServiceFactory(
+          configService.get<string>('JWT_REFRESH_TOKEN_SECRET'),
+        );
+      },
+      inject: [ConfigService],
+    },
+  ],
 })
-export class AuthModule { }
+export class AuthModule {}
